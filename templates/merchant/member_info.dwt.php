775e408a16dbb1253936b222417ee5b15efa55c5
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-11-02 10:23:38 +0800

    更新会员详情样式

--- templates/merchant/member_info.dwt.php
+++ templates/merchant/member_info.dwt.php
@@ -15,7 +15,7 @@
     border:none;
     padding-right: 0;
 }
-.user_name_tr td div { height:36px; line-height:36px;}
+.user_name_tr td div,.user_name_tr td div { height:36px; line-height:36px;}
 .user_name { display:inline-block;}
 </style>
 <div class="page-header">
