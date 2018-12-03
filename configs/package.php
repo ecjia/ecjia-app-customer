2ab40d6bb8ed14be4bdafb7328f6e22831595899
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-12-03 16:26:58 +0800

    更新客户管理语言包

--- configs/package.php
+++ configs/package.php
@@ -6,8 +6,8 @@ defined('IN_ECJIA') or exit('No permission resources.');
 return array(
     'identifier' 	=> 'ecjia.customer',
 	'directory' 	=> 'customer',
-	'name'			=> '客户管理',
-	'description' 	=> '客户管理，添加客户，客户领用、指派。',
+	'name'			=> 'customer',
+	'description' 	=> 'customer_desc',
 	'author' 		=> 'ECJIA TEAM',
 	'website' 		=> 'http://www.ecjia.com',
 	'version' 		=> '1.2.1',
