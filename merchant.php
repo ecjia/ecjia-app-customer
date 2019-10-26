d2a214f54c9104e1785aa2df76d7a7f05e5eeedf
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-10-26 11:19:33 +0800

    修复表格导出

--- merchant.php
+++ merchant.php
@@ -349,7 +349,7 @@ class merchant extends ecjia_merchant {
                     $rank = RC_DB::table('user_rank')->select('rank_id', 'rank_name')->where('rank_id', $rows['user_rank'])->first();
                 }
                 $rows['avatar_img'] = !empty($rows['avatar_img']) ? RC_Upload::upload_url($rows['avatar_img']) : '';
-                $rows['rank_name'] = $rank['rank_name'];
+                $rows['rank_name'] = !empty($rank['rank_name']) ? $rank['rank_name'] : '/';
                 $rows['mobile_phone'] = !empty($rows['mobile_phone']) ? substr_replace($rows['mobile_phone'],'****',3,4) : '';
                 //订单总金额（普通配送订单，不含退款）
                 $rows['buy_amount'] = price_format($rows['buy_amount']);
